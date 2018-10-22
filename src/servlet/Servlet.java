65fae160b315c4045e84eb2d0cdb9e694548804e
KBH <75nstebesb@naver.com>
2018-10-22 21:55:11 +0900

    2

--- src/servlet/Servlet.java
+++ src/servlet/Servlet.java
@@ -36,7 +36,7 @@ public class Servlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		
 		String htmlResponse = "<html>";
-		htmlResponse+="<h2> your name is" + username + "<br/>"; 
+		htmlResponse+="<h2> your name is" + username +"ww"+ "<br/>"; 
 		htmlResponse+=" your password is " + password + "<h2/>";
 		htmlResponse+="</html>";
 		
